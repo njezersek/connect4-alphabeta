3c74ab4f079696b58835c2f5b459b12546c038b7
Nejc Jezeršek <njezersek@gmail.com>
2022-02-14 17:07:38 +0100

    initial setup

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	export let name: string;
+</script>
+
+<main>
+	<h1>Connect 4</h1>
+	<div></div>
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		max-width: 1000px;
+		margin: 0 auto;
+		height: 100%;
+	}
+
+	div {
+		background-color: aquamarine;
+		height: 100%;
+	}
+
+	h1 {
+		
+	}
+</style>
